1d4f25d510904faaee6c0a8d85ebcd423d7adea0
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2023-12-19 22:10:21 +0100

    Dlt, old library to manage riscv

--- examples/onemax/onemax-riscv/onemax-riscv.py
+++ examples/onemax/onemax-riscv/onemax-riscv.py
@@ -16,23 +16,11 @@ from typing import Sequence
 
 import byron
 import library_riscv as library
-from riscv.riscv_evaluator import RiscvEvaluator
-from riscv.register_map import RegisterMap
-
-
-def evaluation_function(registers: Sequence[int]) -> int:
-    return sum(int(i) for i in "{0:b}".format(registers[0]))
 
 
 def main():
     top_frame = library.define_frame()
-    register_map = RegisterMap()
-    register_map.include_registers(("t0",))
-    riscv = RiscvEvaluator(register_map, evaluation_func=evaluation_function)
 
-    # evaluator = byron.evaluator.MakefileEvaluator(
-    #     'onemax.s', stdout_cleaner=riscv.evaluate_riscv_int_stdout, make_flags=["name=onemax", "-s"], timeout=5
-    # )
     evaluator = byron.evaluator.MakefileEvaluator(
         'onemax.s', required_files=['main.c'], make_flags=["name=onemax", "-s"], timeout=5
     )
