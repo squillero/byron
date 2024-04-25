75537e03ba72844da49b1f20330d3f01caa65d49
Giovanni Squillero <squillero@polito.it>
2024-04-25 15:57:10 +0500

    OpenAI Token is not set.

--- utils/default-script.py
+++ utils/default-script.py
@@ -8,3 +8,20 @@
 #################################### ' #####################################
 # Copyright 2023-24 Giovanni Squillero and Alberto Tonda
 # SPDX-License-Identifier: Apache-2.0
+
+
+def is_prime(number):
+    """Check if `number` is prime.
+
+    Args:
+        number (int): the number to check
+
+    Returns:
+        bool: `True` if `number` is prime, `False` otherwise
+    """
+    if number < 2:
+        return False
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            return False
+    return True
