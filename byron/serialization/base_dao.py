efee3e0ef2c92f8259375bbf5b45a77aaf5e7c52
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2024-05-16 12:49:39 +0200

    Add, base_dao.py

---
+++ byron/serialization/base_dao.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from abc import abstractmethod, ABC
+from typing import Type, Any, Optional
+
+from lxml import objectify, etree
+from lxml.objectify import ObjectifiedElement
+
+
+class BaseDAO(ABC):
+    @abstractmethod
+    def objectify(self) -> ObjectifiedElement:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def deobjectify(
+        data: ObjectifiedElement, dao_type: Type[BaseDAO] = Type['BaseDAO'], tag: Optional[str] = None
+    ) -> BaseDAO:
+        pass
+        # if type(data) is str:
+        #     return objectify.fromstring(data)
+        # return data
+
+    @staticmethod
+    @abstractmethod
+    def from_object(obj: Any, tag: Optional[str] = None) -> BaseDAO:
+        pass
+
+    def serialize(self, deannotate=True) -> str:
+        objectified = self.objectify()
+        if deannotate:
+            objectify.deannotate(objectified)
+        xml = etree.tostring(objectified, pretty_print=True, encoding="unicode")
+        return xml
+
+    @classmethod
+    def deserialize(cls, xml: str) -> BaseDAO:
+        objectified = objectify.fromstring(xml)
+        return cls.deobjectify(objectified)
