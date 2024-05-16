7913ed943800513f12d4149f5772d242ec087ca4
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2024-05-16 17:19:10 +0200

    Add, list_dao.py

---
+++ byron/serialization/list_dao.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import TypeVar, Generic, List, Optional, Type
+
+from lxml import objectify
+from lxml.objectify import ObjectifiedElement
+
+from .base_dao import BaseDAO
+
+T = TypeVar('T')
+
+
+class ListDAO(BaseDAO, Generic[T]):
+    _tag: str = "list"
+    _list: List[T]
+
+    def __init__(self, list: List[T], tag: Optional[str] = None):
+        self._list = list
+        if tag is not None:
+            self._tag = tag
+
+    # @staticmethod
+    # def from_obj(tag: str, list: List[T]) -> ListDAO[T]:
+    #     return ListDAO(tag, list)
+
+    @staticmethod
+    def from_object(obj: List[T], tag: Optional[str] = None) -> ListDAO[T]:
+        return ListDAO(obj, tag)
+
+    def objectify(self) -> ObjectifiedElement:
+        l = objectify.Element(self._tag)
+        for element in self._list:
+            l.append(element.objectify())
+        return l
+
+    @staticmethod
+    def deobjectify(data: ObjectifiedElement, dao_type: Type[BaseDAO] = T, tag: Optional[str] = None) -> ListDAO[T]:
+        # raise error if tag or daotype are none
+        return ListDAO([dao_type.deobjectify(d) for d in data.getchildren()], tag)
