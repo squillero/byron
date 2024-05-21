122d94ed7352f527062654239f969e4888238a4d
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2024-05-21 15:13:37 +0200

    Add, individual_dao.py

---
+++ byron/serialization/individual_dao.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import Optional, Type
+from lxml import objectify
+from lxml.objectify import ObjectifiedElement
+
+from byron.classes import Individual
+from .base_dao import BaseDAO
+from .genome_dao import GenomeDAO
+
+
+class IndividualDAO(BaseDAO):
+    _tag: str = "individual"
+    _id: int
+    _genome: GenomeDAO
+
+    def __init__(self, id: int, genome: GenomeDAO, tag: Optional[str] = None) -> None:
+        self._id = id
+        self._genome = genome
+        if tag is not None:
+            self._tag = tag
+
+    # @staticmethod
+    # def from_individual(individual: Individual) -> IndividualDAO:
+    #     return IndividualDAO(individual.id, GenomeDAO.from_genome(individual.genome))
+
+    @staticmethod
+    def from_object(obj: Individual, tag: Optional[str] = None) -> IndividualDAO:
+        return IndividualDAO(obj.id, GenomeDAO.from_object(obj.genome))
+
+    def objectify(self) -> ObjectifiedElement:
+        individual = objectify.Element(self._tag)
+        individual.set("id", str(self._id))
+        individual.append(self._genome.objectify())
+        return individual
+
+    @staticmethod
+    def deobjectify(
+        data: ObjectifiedElement, dao_type: Optional[Type[BaseDAO]] = Type['IndividualDAO'], tag: Optional[str] = None
+    ) -> IndividualDAO:
+        return IndividualDAO(data.get("id"), GenomeDAO.deobjectify(data.genome), tag)
