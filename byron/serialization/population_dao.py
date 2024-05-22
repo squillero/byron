d835ea1345d70061a2b42578e745365de4bf9e1a
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2024-05-22 14:59:13 +0200

    Upd, population_dao.py

--- byron/serialization/population_dao.py
+++ byron/serialization/population_dao.py
@@ -22,30 +22,30 @@ class PopulationDAO(BaseDAO):
         if tag is not None:
             self._tag = tag
 
-    # @staticmethod
-    # def from_population(population: Population) -> PopulationDAO:
-    #     individuals = [IndividualDAO.from_individual(I) for i, I in population]
-    #     return PopulationDAO(population.generation, ListDAO.from_object(individuals, "individuals"))
+    def __str__(self):
+        return f"{self._tag} generation:{self._generation} => {str(self._individuals)}"
+
+    @property
+    def generation(self):
+        return self._generation
+
+    @property
+    def individuals(self):
+        return self._individuals
 
     @staticmethod
     def from_object(obj: Population, tag: Optional[str] = None) -> PopulationDAO:
         individuals = [IndividualDAO.from_object(I) for i, I in obj]
         return PopulationDAO(obj.generation, ListDAO.from_object(individuals, "individuals"))
 
-    # @staticmethod
-    # def deserialize(data: ObjectifiedElement) -> PopulationDAO:
-    #     return PopulationDAO(data.get("generation"), None)
+    @staticmethod
+    def deobjectify(
+        data: ObjectifiedElement, dao_type: Optional[Type[BaseDAO]] = Type['PopulationDAO'], tag: Optional[str] = None
+    ) -> PopulationDAO:
+        return PopulationDAO(data.generation, ListDAO.deobjectify(data.individuals, IndividualDAO, "individuals"), tag)
 
     def objectify(self) -> ObjectifiedElement:
         population = objectify.Element(self._tag)
         population.generation = str(self._generation)
         population.append(self._individuals.objectify())
         return population
-
-    @staticmethod
-    def deobjectify(
-        data: ObjectifiedElement, dao_type: Optional[Type[BaseDAO]] = Type['PopulationDAO'], tag: Optional[str] = None
-    ) -> PopulationDAO:
-        return PopulationDAO(data.generation, ListDAO.deobjectify(data.individuals, IndividualDAO, "individuals"), tag)
-        # call sub deobjectify
-        # return pop dao
