964d42d084c8f39b10174dce898a8a5d89b4279d
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2024-05-22 14:11:45 +0200

    Upd, node_dao.py

--- byron/serialization/node_dao.py
+++ byron/serialization/node_dao.py
@@ -21,12 +21,16 @@ class NodeDAO(BaseDAO):
         if tag is not None:
             self._tag = tag
 
-    # @staticmethod
-    # def from_out_edges(id: str, out_edges: Sequence[Tuple[str, str, int, Dict[str, str]]]) -> NodeDAO:
-    #     edges = []
-    #     for u, v, k, d in out_edges:
-    #         edges.append(EdgeDAO.from_edge(v, k, d))
-    #     return NodeDAO(id, ListDAO.from_object(edges, "edges"))
+    def __str__(self):
+        return f"{self._tag} id:{self._id} => {str(self._out_edges)}"
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def out_edges(self):
+        return self._out_edges
 
     @staticmethod
     def from_object(
@@ -36,14 +40,14 @@ class NodeDAO(BaseDAO):
         edges = [EdgeDAO.from_object((v, k, d)) for u, v, k, d in obj[1]]
         return NodeDAO(id, ListDAO.from_object(edges, "edges"))
 
-    def objectify(self) -> ObjectifiedElement:
-        node = objectify.Element(self._tag)
-        node.set("id", str(self._id))
-        node.append(self._out_edges.objectify())
-        return node
-
     @staticmethod
     def deobjectify(
         data: ObjectifiedElement, dao_type: Optional[Type[BaseDAO]] = Type['NodeDAO'], tag: Optional[str] = None
     ) -> NodeDAO:
         return NodeDAO(data.get("id"), ListDAO.deobjectify(data.edges, EdgeDAO, "edges"), tag)
+
+    def objectify(self) -> ObjectifiedElement:
+        node = objectify.Element(self._tag)
+        node.set("id", str(self._id))
+        node.append(self._out_edges.objectify())
+        return node
