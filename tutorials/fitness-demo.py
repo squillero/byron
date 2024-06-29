98f1d8d039628bd7c216b9032db20d057cf731fc
Giovanni Squillero <squillero@polito.it>
2024-06-29 09:23:36 +0200

    style: Global refactoring

--- tutorials/fitness-demo.py
+++ tutorials/fitness-demo.py
@@ -3,13 +3,15 @@
 #   _____                          |   |                                   #
 #  |  __ \--.--.----.-----.-----.  |===|  This file is part of Byron, an   #
 #  |  __ <  |  |   _|  _  |     |  |___|  evolutionary source-code fuzzer. #
-#  |____/ ___  |__| |_____|__|__|   ).(   -- v0.8a1 "Don Juan"             #
+#  |____/ ___  |__| |_____|__|__|   ).(   Version 0.8a1 "Don Juan"         #
 #        |_____|                    \|/                                    #
 #################################### ' #####################################
 # Copyright 2023-24 Giovanni Squillero and Alberto Tonda
 # SPDX-License-Identifier: Apache-2.0
 
 from math import sqrt
+
+from icecream import ic
 
 import byron
 
@@ -26,23 +28,25 @@ MAGIC_NUMBERS = [
 ]
 
 
-def test(type_, n1, n2):
+def run_comparison(type_, n1, n2):
     f1 = type_(n1)
     f2 = type_(n2)
-    print(f"* {type_.__name__:<18s} :: {f1} == {f2}: {f1 == f2}")
-    print(f"* {type_.__name__:<18s} :: {f1} < {f2}: {f1 < f2}")
-    print(f"* {type_.__name__:<18s} :: {f1} > {f2}: {f1 > f2}")
-    print(f"* {type_.__name__:<18s} :: {f1} << {f2}: {f1 << f2}")
-    print(f"* {type_.__name__:<18s} :: {f1} >> {f2}: {f1 >> f2}")
+    ic(f1 == f2)
+    ic(f1 < f2)
+    ic(f1 > f2)
+    ic(f1 << f2)
+    ic(f1 >> f2)
 
 
 def main():
     for d, n1, n2 in MAGIC_NUMBERS:
-        print(f"\n# {d}")
+        ic()
         for type_ in FITNESS_TYPES:
-            test(type_, n1, n2)
-        for type_ in FITNESS_TYPES:
-            test(byron.fit.reverse_fitness(type_), n1, n2)
+            ic(d, type_)
+            run_comparison(type_, n1, n2)
+            type2_ = byron.fit.reverse_fitness(type_)
+            ic(d, type2_)
+            run_comparison(type2_, n1, n2)
 
 
 if __name__ == "__main__":
