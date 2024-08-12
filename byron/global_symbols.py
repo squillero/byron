2d2ee262ca6885ce813821116001f989c59130c1
Giovanni Squillero <squillero@polito.it>
2024-08-12 10:57:51 +0200

    style(bumpver): Bump version to 0.8a1.dev57

--- byron/global_symbols.py
+++ byron/global_symbols.py
@@ -63,8 +63,8 @@ import multiprocessing
 import sys
 from collections import defaultdict
 
-__version__ = "0.8a1.dev56"
-__date__ = "11-08-2024"
+__version__ = "0.8a1.dev57"
+__date__ = "12-08-2024"
 __codename__ = "Don Juan"
 __author__ = "Giovanni Squillero and Alberto Tonda"
 __copyright__ = "Copyright (c) 2023-24 Giovanni Squillero and Alberto Tonda"
